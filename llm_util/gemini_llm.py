fa4b32c5217af9f620ff1f57c35a4ac439192a51
nitingopi <nitin.gopi@gmail.com>
2024-09-24 16:03:32 +0530

    modified L1 for gemini

--- llm_util/gemini_llm.py
+++ llm_util/gemini_llm.py
@@ -3,6 +3,7 @@ import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 # from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
+from IPython.display import display, Markdown
 
 
 from config.config import GEMINI_API_KEY
@@ -42,9 +43,15 @@ class Gemini_llm(LLMBase):
         output = model.generate_content(prompt, generation_config=config )
         return output.text
     
+    def get_markdown_response(self, prompt: str):
+        response = self.get_response(prompt)
+        return display(Markdown(response))
+    
+
     def to_markdown(self, text: str) -> Markdown:
-        text = text.replace('•', '  *')
-        return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+        display(Markdown(text))
+        # text = text.replace('•', '  *')
+        # return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
     
 
     def initialize_conversation(self, memory) -> ConversationChain:
